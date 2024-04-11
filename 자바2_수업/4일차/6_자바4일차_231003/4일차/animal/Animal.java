1d15720468e1c91b1e71576e7d22b6ae060b4862
jooyeon <victoai999@gmail.com>
2024-04-11 23:48:18 +0900

    0411

--- 자바2_수업/4일차/6_자바4일차_231003/4일차/animal/Animal.java
+++
@@ -1,7 +0,0 @@
-package java2.day4.변경이유리한코드.animal;
-
-public class Animal {	
-	public void bark() {  
-		  
-	}
-}
--- 자바2_수업/4일차/6_자바4일차_231003/4일차/계산기/Calculator.java
+++
@@ -1,7 +0,0 @@
-package java2.day4.변경이유리한코드.계산기;
-
-public interface Calculator {
-	
-	int add( int su1, int su2);
-
-}
